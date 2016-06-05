1d388d67c6e9c0643384fbd9a4b50eabea540fb2
AECUTE <kampee.expertly01@gmail.com>
2016-06-05 13:05:43 +0700

    upspeed

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -38,15 +38,6 @@ class HomeController < ApplicationController
 			redirect_to "/login"
 		end
 
-		 # value for html
-		 @time = Array["0600","0700","0800","0900","1000","1100","1200","1300","1400","1500","1600","1700","1800","1900","2000","2100","2200","2300","2400"]
-		 @day = Array["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-		 @day1 = Array["M","Tu","W", "Th","F","Sa","Su"]
-		 @day2 = Array["Mo","T","We", "Th","Fr","Sa","Su"]
-		 @color = Array["#FF6138","#DA9844","#2B4C8C","#644D52","#00A388","#F25F5C","#247BA0","#A6937C","#332532","#0D1326"]
-		 @color_tab = "#212121"
-		 @color_blank = "#424242"
-
 		semester=session[:semester]
 		year=session[:year]
 		sid=session[:sid]
@@ -153,87 +144,28 @@ class HomeController < ApplicationController
 			end
 		end
 
-		 # order times
-		 i=0
-		 j=0
-		 while i < @timetables.length
-		 	while j < @timetables.length
-		 		if @timetables[i][0,4] < @timetables[j][0,4]
-		 			@timetables[i], @timetables[j] = @timetables[j], @timetables[i]
-		 		end
-		 		j+=1
-		 	end
-		 	i+=1
-		 	j=0
-		 end
 
-		 # find MAX AND MIN TIME TO STUDY
-		 @begin_time = "2400"
-		 @end_time = "0600"
-
-		 @timetables.each do |t|
-		 	times = t.split("\0")
-		 	if times[0] < @begin_time
-		 		@begin_time=times[0]
-		 	end
-
-		 	if times[1] > @end_time
-		 		@end_time=times[1]
-		 	end
-
-		 end
-
-		 # set course for color
-		 @courses = Array.new
-		 @timetables.each do |t|
-		 	i=0
-		 	add = true
-		 	course = t.split("\0")
-
-		 	if @courses.length == 0
-		 		@courses << course[4]
-		 	else
-		 		while i<@courses.length do
-		 			if @courses[i] == course[4]
-		 				add = false
-		 				break
-		 			end
-		 			i+=1
-		 		end
-		 		if add
-		 			@courses << course[4]
-		 		end
-		 	end
-		 end
-
-		 # @courses.each do |c|
-		 # 	puts c
-		 # 	puts @courses.index(c)
-		 # end
-
-
-		
 		end
 
 	def exam
-			if session[:semester] == nil
-				redirect_to "/login"
-			end
+		if session[:semester] == nil
+			redirect_to "/login"
+		end
 
-			semester=session[:semester]
-			year=session[:year]
-			sid=session[:sid]
+		semester=session[:semester]
+		year=session[:year]
+		sid=session[:sid]
 
-			@color_tab = "#212121"
-			@color_blank = "#424242"
+		@color_tab = "#212121"
+		@color_blank = "#424242"
 
 
-			url = "https://www3.reg.cmu.ac.th/regist" + semester.to_s+year.to_s + "/public/result.php?id=" + sid.to_s
-			uri = URI.parse("https://www3.reg.cmu.ac.th/regist" + semester.to_s+year.to_s + "/public/search.php?act=search")
-			url_final = "https://www3.reg.cmu.ac.th/regist/public/exam.php?type=FINAL&term="+semester.to_s+year.to_s
+		url = "https://www3.reg.cmu.ac.th/regist" + semester.to_s+year.to_s + "/public/result.php?id=" + sid.to_s
+		uri = URI.parse("https://www3.reg.cmu.ac.th/regist" + semester.to_s+year.to_s + "/public/search.php?act=search")
+		url_final = "https://www3.reg.cmu.ac.th/regist/public/exam.php?type=FINAL&term="+semester.to_s+year.to_s
 
-			infor = Nokogiri::HTML(open(url))
-			data = infor.css('.msan8')
+		infor = Nokogiri::HTML(open(url))
+		data = infor.css('.msan8')
 
 		#regular data
 		regular = Nokogiri::HTML(open(url_final))
