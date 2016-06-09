1b18aef8c637bef87c01e62874262c1b86090b25
AECUTE <kampee.expertly01@gmail.com>
2016-06-09 12:33:02 +0700

    inline css

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -42,6 +42,15 @@ class HomeController < ApplicationController
 		year=session[:year]
 		sid=session[:sid]
 
+		# value for html
+		 @time = Array["0600","0700","0800","0900","1000","1100","1200","1300","1400","1500","1600","1700","1800","1900","2000","2100","2200","2300","2400"]
+		 @day = Array["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+		 @day1 = Array["M","Tu","W", "Th","F","Sa","Su"]
+		 @day2 = Array["Mo","T","We", "Th","Fr","Sa","Su"]
+		 @color = Array["#FF6138","#DA9844","#2B4C8C","#644D52","#00A388","#F25F5C","#247BA0","#A6937C","#332532","#0D1326"]
+		 @color_tab = "#212121"
+		 @color_blank = "#424242"
+
 		#index for find room
 		if year.to_i<58
 			index=7
@@ -57,6 +66,7 @@ class HomeController < ApplicationController
 		infor = Nokogiri::HTML(open(url))
 		data = infor.css('.msan8')
 		@timetables = Array.new
+
 		
 
 		data.each do |d|
@@ -202,14 +212,7 @@ class HomeController < ApplicationController
 		 # 	puts @courses.index(c)
 		 # end
 
-		 # value for html
-		 @time = Array["0600","0700","0800","0900","1000","1100","1200","1300","1400","1500","1600","1700","1800","1900","2000","2100","2200","2300","2400"]
-		 @day = Array["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
-		 @day1 = Array["M","Tu","W", "Th","F","Sa","Su"]
-		 @day2 = Array["Mo","T","We", "Th","Fr","Sa","Su"]
-		 @color = Array["#FF6138","#DA9844","#2B4C8C","#644D52","#00A388","#F25F5C","#247BA0","#A6937C","#332532","#0D1326"]
-		 @color_tab = "#212121"
-		 @color_blank = "#424242"
+		 
 
 
 		end
