a612a3cfeca943d99775f420594d4fcc39b619a2
aquzohr <kampee.expertly01@gmail.com>
2018-08-16 12:27:58 +0700

    add blogger

---
+++ app/views/bloggers/_blogger.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! blogger, :id, :title, :des, :created_at, :updated_at
+json.url blogger_url(blogger, format: :json)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180815081014) do
+ActiveRecord::Schema.define(version: 20180816034619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,14 @@ ActiveRecord::Schema.define(version: 20180815081014) do
     t.datetime "updated_at",                          null: false
     t.index ["email"], name: "index_admins_on_email", unique: true, using: :btree
     t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "bloggers", force: :cascade do |t|
+    t.string   "title"
+    t.text     "des"
+    t.text     "reference"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "welcomes", force: :cascade do |t|
